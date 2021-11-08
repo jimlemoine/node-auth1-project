fa30cf439b7675e04c0ab0315ed2cf69e35ad4ed
jimlemoine <jlemoine007@gmail.com>
2021-11-08 12:15:19 -0800

    created auth middlewares

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,4 @@
+const User = require('../users/users-model');
 /*
   If the user does not have a session saved in the server
 
@@ -6,8 +7,12 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next()
+  } else {
+    next({ status: 401, message: "You shall not pass!" })
+  }
 }
 
 /*
@@ -18,8 +23,13 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
+function checkUsernameFree(req, res, next) {
+  const user = User.find().find(user => req.body.username === user.username)
+  if (user) {
+    next({ status: 422, message: "Username taken" })
+  } else {
+    next()
+  }
 }
 
 /*
@@ -30,8 +40,13 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+function checkUsernameExists(req, res, next) {
+  const user = User.find().find(user => req.body.username === user.username)
+  if (!user) {
+    next({ status: 401, message: "Invalid credentials" })
+  } else {
+    next()
+  }
 }
 
 /*
@@ -42,8 +57,19 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+function checkPasswordLength(req, res, next) {
+  const { password } = req.body.password;
+  if (!password || password.trim().length <= 3) {
+    next({ status: 422, message: "Password must be longer than 3 chars" })
+  } else {
+    next()
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength
+}
